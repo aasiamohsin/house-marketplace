dcfb2b49c146352e3a119405b7357009665bc83c
Aasia Mohsiin <aasiamohsin.91@gmail.com>
2022-04-23 02:00:41 +0500

    Explore Component Modified

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,3 +1,36 @@
+import { Link } from 'react-router-dom';
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
+
 export const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className='explore'>
+      <header>
+        <p className='pageHeader'>Explore</p>
+      </header>
+      <main>
+        {/* Slider */}
+
+        <p className='exploreCategoryHeading'>Categories</p>
+        <div className='exploreCategories'>
+          <Link to='/category/rent'>
+            <img
+              className='exploreCategoryImg'
+              src={rentCategoryImage}
+              alt='rent'
+            />
+            <p className='exploreCategoryName'>Places for Rent</p>
+          </Link>
+          <Link to='/category/sell'>
+            <img
+              className='exploreCategoryImg'
+              src={sellCategoryImage}
+              alt='sell'
+            />
+            <p className='exploreCategoryName'>Places for Sell</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 };
