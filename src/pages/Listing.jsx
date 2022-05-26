258a047d1e15cdca8d0c3b28fde7973680fdc5b8
Aasia Mohsiin <aasiamohsin.91@gmail.com>
2022-05-26 21:29:03 +0500

    Added Slider Component
    Added images slider in explore page and onClick navigate it to listing.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -17,7 +17,6 @@ export const Listing = () => {
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   const params = useParams();
-  const navigate = useNavigate();
   const auth = getAuth();
 
   useEffect(() => {
@@ -26,7 +25,6 @@ export const Listing = () => {
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log(docSnap.data());
         setListing(docSnap.data());
         setLoading(false);
       }
---
+++ src/pages/Slider.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { collection, getDocs, limit, query, orderBy } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import { Spinner } from '../components/Spinner';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+export const Slider = () => {
+  const [loading, setLoading] = useState(true);
+  const [listing, setListing] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const listingRef = collection(db, 'listings');
+      const q = query(listingRef, orderBy('timestamp', 'desc', limit(5)));
+      const querySnap = await getDocs(q);
+
+      let listing = [];
+
+      querySnap.forEach((doc) => {
+        return listing.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListing(listing);
+      setLoading(false);
+    };
+
+    fetchListing();
+  }, []);
+
+  if (loading) return <Spinner />;
+
+  return (
+    listing && (
+      <>
+        <p className='exploreHeading'>Recommended</p>
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+          {listing.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                style={{
+                  background: `url(${data.imgUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='swiperSlideDiv'
+              >
+                <p className='swiperSlideText'>{data.name}</p>
+                <p className='swiperSlidePrice'>
+                  ${data.discountedPrics ?? data.regularPrice}
+                  {data.type === 'rent' && '/month'}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
+};
